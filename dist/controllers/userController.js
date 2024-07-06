837acb7ec7ad022226387cb9aee4688aa4fe00f0
Phenzic <juliusayoola5@gmail.com>
2024-07-06 12:19:16 +0100

    build

--- dist/controllers/userController.js
+++ dist/controllers/userController.js
@@ -1,18 +1,63 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteUser = exports.updateUser = exports.createUser = exports.getUsers = void 0;
+exports.deleteUser = exports.createUser = exports.updateUserProfile = exports.getUserById = exports.getUsers = exports.signIn = exports.signUp = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const uuid_1 = require("uuid");
 const userModel_1 = require("../models/userModel");
+const config_1 = __importDefault(require("../config"));
+const authMiddleWare_1 = require("../config/authMiddleWare");
+const secretKey = config_1.default.JWT_SECRET;
+const signUp = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { name, email, password } = req.body;
+    const hashedPassword = yield (0, userModel_1.hashPassword)(password);
+    const newUser = { id: (0, uuid_1.v4)(), name, email, password: hashedPassword };
+    userModel_1.users.push(newUser);
+    const token = jsonwebtoken_1.default.sign({ id: newUser.id }, secretKey, { expiresIn: '1h' });
+    res.status(201).json({ token });
+});
+exports.signUp = signUp;
+const signIn = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { email, password } = req.body;
+    const user = userModel_1.users.find((u) => u.email === email);
+    if (user && (yield (0, userModel_1.comparePassword)(password, user.password))) {
+        const token = jsonwebtoken_1.default.sign({ id: user.id }, secretKey, { expiresIn: '1h' });
+        res.json({ token });
+    }
+    else {
+        res.status(401).send('Invalid email or password');
+    }
+});
+exports.signIn = signIn;
 const getUsers = (req, res) => {
     res.json(userModel_1.users);
 };
 exports.getUsers = getUsers;
+const getUserById = (req, res) => {
+    res.json(authMiddleWare_1.fetchUser);
+};
+exports.getUserById = getUserById;
+// const getUserById = (id: string): User | undefined => {
+//   return users.find(user => user.id === id);
+// };
 const createUser = (req, res) => {
     const newUser = req.body;
     userModel_1.users.push(newUser);
     res.status(201).json(newUser);
 };
 exports.createUser = createUser;
-const updateUser = (req, res) => {
+const updateUserProfile = (req, res) => {
     const id = req.params.id;
     const updatedUser = req.body;
     const userIndex = userModel_1.users.findIndex((user) => user.id === id);
@@ -24,7 +69,7 @@ const updateUser = (req, res) => {
         res.status(404).send("User not found");
     }
 };
-exports.updateUser = updateUser;
+exports.updateUserProfile = updateUserProfile;
 const deleteUser = (req, res) => {
     const id = req.params.id;
     const userIndex = userModel_1.users.findIndex((user) => user.id === id);
--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -5,9 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const userController_1 = require("../controllers/userController");
+// import {  } from '../controllers/userController';
+const authMiddleWare_1 = require("../config/authMiddleWare");
 const router = express_1.default.Router();
-router.get("/", userController_1.getUsers);
-router.post("/", userController_1.createUser);
-router.put("/:id", userController_1.updateUser);
-router.delete("/:id", userController_1.deleteUser);
+// Auth routes
+router.post('/signup', userController_1.signUp);
+router.post('/signin', userController_1.signIn);
+// User routes with authentication
+router.get('/', authMiddleWare_1.authenticateJWT, userController_1.getUsers);
+// router.post('/', authenticateJWT, createUser);
+router.get('/me', authMiddleWare_1.authenticateJWT, userController_1.getUserById);
+router.delete('/:id', authMiddleWare_1.authenticateJWT, userController_1.deleteUser);
+router.put('/:id', authMiddleWare_1.authenticateJWT, userController_1.updateUserProfile);
 exports.default = router;
